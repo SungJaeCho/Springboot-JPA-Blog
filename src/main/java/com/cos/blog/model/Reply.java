9e966fb3d2645695e753d5de5f73e22585128a3d
SungJaeCho <liadus@naver.com>
2021-04-23 06:23:00 +0900

    20210423 댓글 네이티브쿼리사용

--- src/main/java/com/cos/blog/model/Reply.java
+++ src/main/java/com/cos/blog/model/Reply.java
@@ -41,6 +41,12 @@ public class Reply {
 	
 	@CreationTimestamp
 	private Timestamp createDate;
+
+	@Override
+	public String toString() {
+		return "Reply [id=" + id + ", content=" + content + ", board=" + board + ", user=" + user + ", createDate="
+				+ createDate + ", toString()=" + super.toString() + "]";
+	}
 	
 //	public void update(User user, Board board, String content) {
 //		setUser(user);
