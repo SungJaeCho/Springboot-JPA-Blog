8883ea12d42c18c674b671c092f09badb9e35f9d
SungJaeCho <liadus@naver.com>
2021-04-24 01:06:46 +0900

    20210424 userservice변경

--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -1,17 +1,12 @@
 package com.cos.blog.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
-import com.cos.blog.repository.ReplyRepository;
 import com.cos.blog.repository.UserRepository;
 
 //스프링이 컴포넌트 스캔을 통해서 Bean에 등록을 해줌. IOC를 해준다.
